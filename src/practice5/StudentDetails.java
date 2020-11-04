c5d1c6f53cdc01fd3f9a38aadcfaf347a463b915
connection579 <wlswn4118@naver.com>
2020-11-04 23:50:10 +0900

    Studentage

---
+++ src/practice5/StudentDetails.java
@@ -0,0 +1,18 @@
+package practice5;
+
+public class StudentDetails {
+	public void StudentAge()
+	{
+		//local variable age
+		int age = 0;
+		age = age +5;
+		System.out.println("Student age is :" + age );
+	}
+
+	public static void main(String[] args) {
+		StudentDetails odj = new StudentDetails();
+		odj.StudentAge();
+
+	}
+
+}
